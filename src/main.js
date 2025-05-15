1d7462516c00ec162b586d0f4a60834ae1e6a8c0
Eduardo Pacheco <epacheco.dev@gmail.com>
2025-05-15 17:31:22 -0300

    feat: add integration form contact

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import './assets/sass/app.sass'
+import './assets/sass/app.sass';
+import 'jquery-mask-plugin';
 
 $(function () {
   const btnNewsletterSubscriberSubmit = $('#btn-newsletter-subscriber-submit');
@@ -21,9 +22,47 @@ $(function () {
         alert('Inscrito com sucesso!')
         btnNewsletterSubscriberSubmit.removeClass('btn-sending').children('span').addClass('d-none')
       }).fail(function (jqXHR, textStatus, msg) {
-        alert('Erro ao realizar a inscrição na Newsletter', msg)
+        alert('Erro ao realizar a inscrição na Newsletter')
         btnNewsletterSubscriberSubmit.removeClass('btn-sending').children('span').addClass('d-none')
       });
   })
+
+  /** Form Contact */
+
+  const btnContactSubmit = $('#btn-contact-submit');
+  const inputContactName = $('#input-contact-name');
+  const inputContactPhoneNumber = $('#input-contact-phone-number');
+  const inputContactEmail = $('#input-contact-email');
+  const inputContactMessage = $('#input-contact-message');
+
+  btnContactSubmit.on('click', () => {
+    $.ajax(
+      {
+        method: 'post',
+        url: 'https://contact.api.bitcriativo.com.br/contact/send',
+        accepts: 'Accept: application/json',
+        data: {
+          name: inputContactName.val(),
+          phoneNumber: inputContactPhoneNumber.val(),
+          email: inputContactEmail.val(),
+          subject: "Formulário de Contato | Site - Bit Criativo",
+          message: inputContactMessage.val()
+        },
+        beforeSend: () => {
+          btnContactSubmit.addClass('btn-sending').children('span').removeClass('d-none')
+        }
+      }).done(() => {
+        alert('Enviado com Sucesso!')
+        btnContactSubmit.removeClass('btn-sending').children('span').addClass('d-none')
+      }).fail(function (jqXHR, textStatus, msg) {
+        console.log(msg)
+        alert('Erro ao enviar a mensagem, por favor, tentar novamente!')
+        btnContactSubmit.removeClass('btn-sending').children('span').addClass('d-none')
+      });
+
+  })
+
+  /** Inputs Masks */
+  $('.input-phone-number-mask').mask('+55 00 90000-0000')
 
 });
