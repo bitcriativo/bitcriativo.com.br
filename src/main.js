28fef3c348d7fd7087622af3afb1816ce6d283be
Eduardo Pacheco <epacheco.dev@gmail.com>
2025-05-15 16:52:51 -0300

    feat: add integration subscriber newsletter

--- src/main.js
+++ src/main.js
@@ -1,2 +1,29 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './assets/sass/app.sass'
+
+$(function () {
+  const btnNewsletterSubscriberSubmit = $('#btn-newsletter-subscriber-submit');
+  const inputNewsletterSubscriberEmail = $('#input-newsletter-subscriber-email');
+
+  btnNewsletterSubscriberSubmit.on('click', () => {
+    $.ajax(
+      {
+        method: 'post',
+        url: 'https://newsletter.api.bitcriativo.com.br/newsletter',
+        accepts: 'Accept: application/json',
+        data: {
+          "email": inputNewsletterSubscriberEmail.val()
+        },
+        beforeSend: () => {
+          btnNewsletterSubscriberSubmit.addClass('btn-sending').children('span').removeClass('d-none')
+        }
+      }).done(() => {
+        alert('Inscrito com sucesso!')
+        btnNewsletterSubscriberSubmit.removeClass('btn-sending').children('span').addClass('d-none')
+      }).fail(function (jqXHR, textStatus, msg) {
+        alert('Erro ao realizar a inscrição na Newsletter', msg)
+        btnNewsletterSubscriberSubmit.removeClass('btn-sending').children('span').addClass('d-none')
+      });
+  })
+
+});
