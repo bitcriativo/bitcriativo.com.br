969bbd0628827e02f725da56aa6d1f6a19b24638
Eduardo Pacheco <epacheco.dev@gmail.com>
2025-05-15 19:44:25 -0300

    feat: create navbar and add bootstrap

--- frontend/resources/views/layouts/main.blade.php
+++ frontend/resources/views/layouts/main.blade.php
@@ -15,6 +15,44 @@
         <title>@yield('title', 'Laravel')</title>
     </head>
     <body>
+        <!-- Navbar START -->
+        <nav class="navbar">
+            <div class="navbar-container">
+                <a class="navbar-brand" href="/">
+                    <img src="{{ asset('assets/images/brand.png') }}" class="d-inline-block align-top" alt="">
+                </a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
+                aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div class="navbar-list collapsed">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ route('home') }}">Início <span class="sr-only">(Atual)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sobre nós</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Serviços</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contato</a>
+                    </li>
+                    </ul>
+                <div class="nav-btns">
+                    <a href="auth/login.html" class="btn btn-ghost">Área do Cliente</a>
+                    <a href="#contato" class="btn btn-primary">Solicitar Orçamento</a>
+                </div>
+            </div>
+        </nav>
         @yield('content')
+
+        <script src="{{ asset("assets/js/bundle.js") }}"></script>
     </body>
 </html>
