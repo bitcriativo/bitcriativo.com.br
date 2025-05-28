a46c72f0a51d8ed910d402b21cf09c1c9cdb16b0
Eduardo Pacheco <epacheco.dev@gmail.com>
2025-05-28 08:03:48 -0300

    feat: create features section portfolio bip hora

--- resources/views/portfolio/bip_hora/home.blade.php
+++ resources/views/portfolio/bip_hora/home.blade.php
@@ -13,4 +13,41 @@
     </header>
     <!-- Hero End -->
 
+    <!-- Features Start -->
+    <section>
+        <h2 class="text-center justify-start text-slate-800 text-5xl font-normal font-sans leading-[55px] tracking-tight">Features</h2>
+        <div class="text-center justify-start text-gray-700 text-3xl font-normal font-sans leading-10 tracking-tight">Most calendars are <br/>designed for teams. </div>
+        <img class="mx-auto" src="{{ asset('bip-hora/assets/images/image_feature_1.png') }}"/>
+
+        <div class="my-5">
+            <img class="mx-auto" src={{ asset('bip-hora/assets/images/icon_feature_1.png') }} />
+            <div class="text-center justify-start text-slate-800 text-xl font-bold font-sans leading-7 tracking-tight">OpenType features <br/>Variable fonts</div>
+            <div class="text-center justify-start text-gray-700 text-lg font-normal font-sans leading-normal tracking-tight">Slate helps you see how <br/>many more days you need <br/>to work to reach your <br/>financial goal.</div>
+        </div>
+
+        <div class="my-5">
+            <img class="mx-auto" src={{ asset('bip-hora/assets/images/icon_feature_2.png') }} />
+            <div class="text-center justify-start text-slate-800 text-xl font-bold font-sans leading-7 tracking-tight">Design with real data</div>
+            <div class="text-center justify-start text-gray-700 text-lg font-normal font-sans leading-normal tracking-tight">Slate helps you see how <br/>many more days you need <br/>to work to reach your <br/>financial goal.</div>
+        </div>
+
+        <div class="my-5">
+            <img class="mx-auto" src={{ asset('bip-hora/assets/images/icon_feature_3.png') }} />
+            <div class="text-center justify-start text-slate-800 text-xl font-bold font-sans leading-7 tracking-tight">Fastest way to <br/>take action</div>
+            <div class="text-center justify-start text-gray-700 text-lg font-normal font-sans leading-normal tracking-tight">Slate helps you see how <br/>many more days you need <br/>to work to reach your <br/>financial goal.</div>
+        </div>
+
+        <div>
+            <div class="text-center justify-start text-slate-800 text-5xl font-normal font-sans leading-[55px] tracking-tight">Fastest way to <br/>organize </div>
+            <div class="text-center justify-start text-gray-700 text-3xl font-normal font-sans leading-10 tracking-tight">Most calendars are <br/>designed for teams. </div>
+            <img src="{{ asset('bip-hora/assets/images/image_feature_2.png') }}" />
+            <div class="flex justify-center">
+                <a href="#" class="mx-auto px-4 py-2 my-4 bg-blue-500 rounded-[35px] flex-col inline-flex items-center gap-2.5 overflow-hidden justify-start text-white text-lg font-normal font-sans leading-tight tracking-tight">
+                    Try For Free
+                </a>
+            </div>
+        </div>
+    </section>
+    <!-- Features End -->
+
 @endsection
