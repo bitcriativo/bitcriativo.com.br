5edb77fae8adc35ab16b5bb904ca93c2fa8da48b
Eduardo Pacheco <epacheco.dev@gmail.com>
2025-06-03 14:46:45 -0300

    feat: create gallery section

--- resources/views/portfolio/architecture/arq_lab/index.blade.php
+++ resources/views/portfolio/architecture/arq_lab/index.blade.php
@@ -20,6 +20,7 @@
         @include('portfolio.architecture.arq_lab.layout.hero')
         @include('portfolio.architecture.arq_lab.layout.about')
         @include('portfolio.architecture.arq_lab.layout.services')
+        @include('portfolio.architecture.arq_lab.layout.photo_gallery')
 
         <!-- Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
