eeba6781f2026b7009d087bf16f0e2778ec20503
Eduardo Pacheco <epacheco.dev@gmail.com>
2025-06-03 09:25:25 -0300

    feat: create sidebar mobile

--- resources/views/portfolio/architecture/arq_lab/layout/navbar.blade.php
+++ resources/views/portfolio/architecture/arq_lab/layout/navbar.blade.php
@@ -17,3 +17,21 @@
         <i class="ph ph-list text-2xl"></i>
     </button>
 </nav>
+
+<div class="overlay__navigation__sidebar"></div>
+
+<nav class="bg-white z-50 fixed top-0 right-0 w-2/3 h-dvh p-4">
+    <div class="flex items-center justify-between">
+        <a href="#" class="">
+            <img src={{ asset('portfolio/arq-lab/assets/image/brand.png') }} />
+        </a>
+        <i class="ph ph-x text-3xl hover:text-primary hover:cursor-pointer"></i>
+    </div>
+    <ul class="space-y-4 text-end py-8 *:hover:text-primary font-sans">
+        <li class=""><a href="{{ route('portfolio.arq_lab') }}">Home</a></li>
+        <li class=""><a href="{{ route('portfolio.arq_lab') }}">About us</a></li>
+        <li class=""><a href="{{ route('portfolio.arq_lab') }}">Our Process</a></li>
+        <li class=""><a href="{{ route('portfolio.arq_lab') }}">Photo Gallery</a></li>
+        <li class=""><a href="{{ route('portfolio.arq_lab') }}">Contact</a></li>
+    </ul>
+</nav>
